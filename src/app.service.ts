1ed6c0b1ca7301ef458dd4586ba302419f4cda04
Young6575 <wnsdud11510@gmail.com>
2025-12-10 15:21:53 +0900

    Test auto deploy with correct username

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! - Auto Deploy Test';
+    return 'Hello World! - Auto Deploy Success with ec2-user';
   }
 }
